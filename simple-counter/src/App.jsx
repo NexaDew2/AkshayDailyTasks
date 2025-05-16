b4983bb7666a5904f2879e64d28a24eae0708cea
AkshayReddy60 <158162320+AkshayReddy60@users.noreply.github.com>
2025-05-16 11:30:16 +0530

    simple counter (#1)
    * simple counter
    
    * toggle mode

---
+++ simple-counter/src/App.jsx
@@ -0,0 +1,25 @@
+
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+  const reset = () => setCount(0);
+
+  return (
+    <div className='container'>
+      <h2>Simple Counter</h2>
+      <h4>Count: {count}</h4>
+      <div className="button">
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+        <button onClick={reset}>Reset</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
